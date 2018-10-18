164b5bbbf697a9d9097fd2ccd6c8521c4c1c6577
Infant Frontender <newbornfrontender@gmail.com>
2018-10-18 12:24:28 +0300

    Переписал код

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const { join } = require('path');
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(join(__dirname + '/index.html'));
 });
 
 app.listen(8080);
